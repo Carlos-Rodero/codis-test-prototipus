99b9074311988d08b28f99a496473e8e081aa39e
Carlos-Rodero <carlos.rodero.garcia@gmail.com>
2019-02-28 20:35:54 +0100

    accelerometer

--- Accelerometer/main.py
+++ Accelerometer/main.py
@@ -3,7 +3,10 @@ from pytrack import Pytrack
 import time
 
 py = Pytrack()
+# init accelerometer
 acc = LIS2HH12()
+
+# print pitch and roll for 100 times
 for i in range(100):
     pitch = acc.pitch()
     roll = acc.roll()
